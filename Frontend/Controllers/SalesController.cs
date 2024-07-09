1ce27a7dd70c9ec6065e71f651dd6bbfa70c029e
mthunzi gcwensa <mthunzigcwensa659@gmail.com>
2024-07-10 01:43:49 +0200

    dcv

---
+++ Frontend/Controllers/SalesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Frontend.Controllers
+{
+    public class SalesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
